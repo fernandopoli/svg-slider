06000c47c038c5a592e19d86fa1b01731c576536
Fernando Poli <fernandoandrespoli@gmail.com>
2019-07-19 09:25:36 -0300

    Adding the landing to the repo.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
-console.log('script');
+// SHARECOUNT
+jQuery.sharedCount = function(url, fn) {
+  url = encodeURIComponent(url || location.href);
+  var domain = "//api.sharedcount.com/v1.0/"; /* SET DOMAIN */
+  var apikey = "b8a9b1129af965899b44aa82fb90d9b99485f894"; /*API KEY HERE*/
+  var arg = {
+    data: {
+      url: url,
+      apikey: apikey
+    },
+    url: domain,
+    cache: true,
+    dataType: "json"
+  };
+  if ("withCredentials" in new XMLHttpRequest()) {
+    arg.success = fn;
+  } else {
+    var cb = "sc_" + url.replace(/\W/g, "");
+    window[cb] = fn;
+    arg.jsonpCallback = cb;
+    arg.dataType += "p";
+  }
+  return jQuery.ajax(arg);
+};
+
+// INIT FUNCTIONS
+jQuery(document).ready(function($) {
+  $.sharedCount(location.href, function(data) {
+    $("#facebook").text(data.Facebook.total_count);
+    $("#linkedin").text(data.LinkedIn);
+    $("#pinterest").text(data.Pinterest);
+    $("#stumbles").text(data.StumbleUpon);
+    $("#comments").text(data.Facebook.comment_count);
+    $("#plusones").text(data.GooglePlusOne);
+    $("#sharedcount").fadeIn();
+  });
+});
---
+++ src/js/slider.js
@@ -0,0 +1,43 @@
+// Auto Play Function
+function autoPlaySlider() {
+  // Global Variables
+  var slideCount = $(".slider-dots li").length - 1;
+  var counter = 0;
+  setInterval(function() {
+    if (counter >= slideCount) {
+      counter = 0;
+    } else {
+      counter += 1;
+    }
+    $('.slider-dots li:eq("' + counter + '") a').click();
+    console.log(counter);
+  }, 8000);
+}
+
+// Slider Change Function
+function SliderChange(e) {
+  e.preventDefault();
+  // Getting the target
+  var linkItem = $(this).data("link");
+  // Removing the active to the selected target
+  $("*[data-link]")
+    .parent()
+    .removeClass("active");
+  // Adding the active to the selected target
+  $('[data-link="' + linkItem + '"]')
+    .parent()
+    .addClass("active");
+  //Changing The image
+  $("*[data-img]").removeClass("active");
+  $('[data-img="' + linkItem + '"]').addClass("active");
+  // Adding Css Animation
+  $(".slider-image-container").removeClass("move");
+  void this.clientWidth;
+  $(".slider-image-container").addClass("move");
+}
+
+// Init Click
+$("*[data-link]").on("click", SliderChange);
+
+// Init AutoPlay
+autoPlaySlider();
